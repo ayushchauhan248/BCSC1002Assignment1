b443ee4667e874e607e0b5f6f2527275fa385dc5
mitali453 <m9319015911>
2020-08-30 13:29:54 +1200

    add mthods to Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -23,4 +23,33 @@ public class Library {
         this.availableBooks = availableBooks;
     }
 
+    /**
+     * This method will help student for checkout a issued a book.
+     *
+     * @param bookName The bookName of the video that the client wants to issue.
+     */
+    public void doCheckout(String bookName) {
+        System.out.println("Thank you for issuing \"" + bookName + "\".");
+    }
+
+    /**
+     * This method will return a issued book to the library.
+     *
+     * @param bookName The bookName of the video that the client wants to return.
+     */
+    public void doReturn(String bookName) {
+        System.out.println(
+                "Thank you for returning \"" + bookName + "\"."
+        );
+    }
+
+    /**
+     * This method prints all the books in the library (store array).
+     */
+    public void listInventory() {
+        for (Book book : availableBooks) {
+            System.out.println(book);
+        }
+    }
+
 }
